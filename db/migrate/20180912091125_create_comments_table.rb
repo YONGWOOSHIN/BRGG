ec364efd089f85b7341e28eb6dda4faaa64f3c41
Yongwoo Shin <ywshin@snu.ac.kr>
2018-11-10 21:29:30 +0900

    Update 20180912091125_create_comments_table.rb

--- db/migrate/20180912091125_create_comments_table.rb
+++ db/migrate/20180912091125_create_comments_table.rb
@@ -2,9 +2,9 @@ class CreateCommentsTable < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
       t.integer :user_id
-      t.integer :date
+      t.date :comment_date
       t.string  :content
-      t.boolean :revision_check
+      #t.boolean :revision_check
     end
   end
 end
