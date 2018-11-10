5155ff5c001ef8e83f6733c353e90386172d20c9
Yongwoo Shin <ywshin@snu.ac.kr>
2018-11-10 21:28:20 +0900

    Update 20180908193443_create_todos_table.rb

--- db/migrate/20180908193443_create_todos_table.rb
+++ db/migrate/20180908193443_create_todos_table.rb
@@ -2,11 +2,11 @@ class CreateTodosTable < ActiveRecord::Migration[5.2]
   def change
     create_table :todos do |t|
       t.integer :user_id
-      t.integer :date
+      t.date :todo_date #date or datetime
       t.string  :content
       t.integer :todo_index
       t.boolean :complete_check
-      t.boolean :revision_check
+      #t.boolean :revision_check
     end
   end
 end
